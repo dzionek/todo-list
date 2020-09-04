820cddeee39d024eed37cfa042ee9b52830f568d
dzionek <vburtoch@gmail.com>
2020-09-05 00:50:34 +0200

    Add Flake8 linting

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -23,4 +24,5 @@ class Task(models.Model):
 
     def __str__(self) -> str:
         finished_message = 'finished' if self.is_finished else 'unfinished'
-        return f'Task {self.id} created by {self.user.username} ({finished_message})'
+        return f'Task {self.id} created by {self.user.username}' \
+               f'({finished_message})'
