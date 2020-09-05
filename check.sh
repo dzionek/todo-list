52e5ea07a99af6ec2126ede0009df34136c374e0
dzionek <vburtoch@gmail.com>
2020-09-05 04:02:39 +0200

    Add local CI checker

---
+++ check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo
+echo "*** --- Linters --- ***"
+echo
+
+echo "1) Mypy"
+mypy .
+echo
+
+echo "2) Flake8"
+flake8
+echo
+
+echo "3) ES-Lint"
+cd frontend
+npm run eslint
+echo
+
+read -p "Do you want to run unit tests [y/n]? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo
+  echo "*** --- Tests --- ***"
+  echo
+
+  echo "4) Jest"
+  npm run test
+  echo
+
+  echo "5) Pytest"
+  echo
+  cd ..
+  pytest --ignore=frontend --cov-config=.coveragerc --cov=.
+fi
